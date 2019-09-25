75cf7e0c74a6fb1fe88186993926264fb2084fb3
Steve <mittosteve@gmail.com>
2019-09-25 11:21:05 +0300

    This commit enables user authentication

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,2 +1,21 @@
 from . import auth
-from flask import render_template
+from flask import render_template,jsonify,request,redirect,url_for
+from app.models import User
+from flask_login import login_user,logout_user,login_required,current_user,is_authenticated
+@auth.route('/login', methods=['POST','GET'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('main.index')
+    else:
+        if request.method == 'POST':
+            form = request.form
+            email = form.get('email')
+            password = form.get('password')
+        user = User.query.filter_by(email = email).first()
+        if user is not None and user.verify_password(password):
+            login_user(user)
+            return jsonify({'success':True})
+        else:
+            return jsonify({'success':False})
+
+        return render_template('auth/login.html')
--- app/static/js/auth/login.js
+++ app/static/js/auth/login.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
+  $('#loginForm').submit(function(event){
+    email = $("#email").vall()
+    password = $("#password").val()
 
+    if ((email == null || "") || (password == null || "") ){
+      $("#error").text("Please fill all fields").fadeIn(500).delay(5000).slideUp(500)
+    }
+
+    $.post('/login',
+    {
+      email:email,
+      password:password,
+    },
+    function(data){
+      if(data.success == true){
+        window.replace.location('/')
+      }
+      if(data.success == false){
+        $("#error").text('Invalid email or password').fadeIn(500).delay(5000).slideUp(500)
+      }
+    });
+    event.preventDefault()
+  });
 });
